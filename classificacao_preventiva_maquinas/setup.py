d78cef85e1b5bd6afedb2ebfe4ddc7911f70b9fe
Johnny <101834686+Johnny-DF26@users.noreply.github.com>
2023-11-28 16:53:32 -0300

    .update

---
+++ classificacao_preventiva_maquinas/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Como os conjuntos de dados reais de manutenção preditiva são geralmente difíceis de obter e, em particular, difíceis de publicar, apresentamos e fornecemos um conjunto de dados sintético que reflete a manutenção preditiva real encontrada na indústria, até onde sabemos.',
+    author='Johnny',
+    license='MIT',
+)
